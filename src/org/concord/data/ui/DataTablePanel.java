9dd5537ee96b15e37ab3226e172cffeafe3ec68b
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2004-08-26 20:43:29 +0000

    More changes for the data table. Added DataStoreEvent and Listener so objects can listen to data store add, change and remove events The table is still not ready
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@1452 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/ui/DataTablePanel.java
+++ src/org/concord/data/ui/DataTablePanel.java
@@ -1,7 +1,7 @@
 /*
  * Last modification information:
- * $Revision: 1.1 $
- * $Date: 2004-08-24 23:15:07 $
+ * $Revision: 1.2 $
+ * $Date: 2004-08-26 20:43:28 $
  * $Author: imoncada $
  *
  * Licence Information
@@ -10,10 +10,15 @@
 package org.concord.data.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 
 
 /**
@@ -26,10 +31,11 @@ import javax.swing.JTable;
  *
  */
 public class DataTablePanel extends JPanel
+	implements TableModelListener, ComponentListener
 {
 	protected JTable table;
 	protected DataTableModel tableModel;
-	
+	protected JScrollPane scrollPane;
 	/**
 	 * 
 	 */
@@ -39,10 +45,15 @@ public class DataTablePanel extends JPanel
 		
 		tableModel = new DataTableModel();
 		table = new JTable(tableModel);
-		table.setBackground(Color.pink);
-		JScrollPane scrollPane = new JScrollPane(table);
+		//table.setBackground(Color.pink);
+		scrollPane = new JScrollPane(table);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		add(scrollPane);
+				
+		tableModel.addTableModelListener(this);
+		
+		addComponentListener(this);
 	}
 
 	
@@ -68,5 +79,68 @@ public class DataTablePanel extends JPanel
 	public JTable getTable()
 	{
 		return table;
+	}
+	/**
+	 * @return Returns the scrollPane.
+	 */
+	public JScrollPane getScrollPane()
+	{
+		return scrollPane;
+	}
+	/**
+	 * @param scrollPane The scrollPane to set.
+	 */
+	public void setScrollPane(JScrollPane scrollPane)
+	{
+		this.scrollPane = scrollPane;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see javax.swing.event.TableModelListener#tableChanged(javax.swing.event.TableModelEvent)
+	 */
+	public void tableChanged(TableModelEvent e)
+	{
+		//Scroll down automatically
+		table.scrollRectToVisible(table.getCellRect(table.getRowCount()-1, -1, true));	
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ComponentListener#componentHidden(java.awt.event.ComponentEvent)
+	 */
+	public void componentHidden(ComponentEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ComponentListener#componentMoved(java.awt.event.ComponentEvent)
+	 */
+	public void componentMoved(ComponentEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ComponentListener#componentResized(java.awt.event.ComponentEvent)
+	 */
+	public void componentResized(ComponentEvent e)
+	{
+		table.setPreferredScrollableViewportSize(new Dimension(getSize().width - 20,getSize().height - 80));
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ComponentListener#componentShown(java.awt.event.ComponentEvent)
+	 */
+	public void componentShown(ComponentEvent e)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 }
