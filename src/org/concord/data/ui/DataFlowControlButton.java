a38952f5e88e3b8cf4a62d58383f0b7db5e97f88
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2004-09-22 16:28:58 +0000

    Added action, button and toolbar that controls automatically a data flow or simulation object, so you can start, stop and reset the simulation
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/data/data@1654 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/data/ui/DataFlowControlButton.java
@@ -0,0 +1,175 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2004-09-22 16:28:58 $
+ * $Author: imoncada $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.data.ui;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+
+import org.concord.framework.data.DataFlow;
+
+
+/**
+ * DataFlowControlButton
+ * Class name and description
+ *
+ * Date created: Sep 20, 2004
+ *
+ * @author imoncada<p>
+ *
+ */
+public class DataFlowControlButton extends JButton
+{
+	/**
+	 * Default constructor
+	 * Creates an empty button that doesn't do anything
+	 */
+	public DataFlowControlButton()
+	{
+		//Empty button
+		this(DataFlowControlAction.FLOW_CONTROL_NONE);
+	}
+	
+	/**
+	 * Creates a button that controls DataFlow objects
+	 * The type specified corresponds to the action that the button
+	 * will have.
+	 * For example, a button with a FLOW_CONTROL_START type,
+	 * will start the data flow objects or simulation objects 
+	 * @param type One of the types defined in DataFlowControlAction
+	 */
+	public DataFlowControlButton(int type)
+	{
+		super();
+		DataFlowControlAction action = new DataFlowControlAction(type);
+		setAction(action);
+	}
+	
+	/**
+	 * Creates a button that will control the object objFlow.
+	 * The type specified corresponds to the action that the button
+	 * will have.
+	 * For example, a button with a FLOW_CONTROL_START type,
+	 * will start the object objFlow. 
+	 * @param objFlow	The data flow or simulation object to control
+	 * @param type 		One of the types defined in DataFlowControlAction
+	 */
+	public DataFlowControlButton(DataFlow objFlow, int type)
+	{
+		this(type);
+		addDataFlowObject(objFlow);
+	}
+	
+	/**
+	 * Creates a button based on an existing DataFlowControlAction action
+	 * @param action
+	 */
+	public DataFlowControlButton(DataFlowControlAction action)
+	{
+		super(action);
+	}
+	
+	/**
+	 * Adds a DataFlow object that will be controlled by this button
+	 * @param objFlow The data flow or simulation object to control
+	 */
+	public void addDataFlowObject(DataFlow objFlow)
+	{
+		getDataFlowControlAction().addDataFlowObject(objFlow);
+	}
+
+	/**
+	 * Removes a DataFlow object that was previously controlled by this button
+	 * @param objFlow The data flow or simulation object to remove and not be controlled anymore
+	 */
+	public void removeDataFlowObject(DataFlow objFlow)
+	{
+		getDataFlowControlAction().removeDataFlowObject(objFlow);
+	}
+
+	/**
+	 * Gets the type of action (start, stop, run)
+	 * @return Returns the flowControlType.
+	 */
+	public int getFlowControlType()
+	{
+		return getDataFlowControlAction().getFlowControlType();
+	}
+	/**
+	 * Sets the type of action (start, stop, run)
+	 * @param flowControlType The flowControlType to set.
+	 * @throws IllegalArgumentException if the type is not one of the
+	 * valid types defined in DataFlowControlAction
+	 */
+	public void setFlowControlType(int type)
+	{
+		getDataFlowControlAction().setFlowControlType(type);
+	}
+
+	/**
+	 * @return Returns the autoEnable.
+	 */
+	public boolean isAutoEnable()
+	{
+		return getDataFlowControlAction().isAutoEnable();
+	}
+	
+	/**
+	 * Sets if the action will be enabled/disabled automatically
+	 * depending on the simulation state of the simulation objects
+	 * @param autoEnable The autoEnable to set.
+	 */
+	public void setAutoEnable(boolean autoEnable)
+	{
+		getDataFlowControlAction().setAutoEnable(autoEnable);
+	}
+	
+	public void addAction(DataFlowControlAction action)
+	{
+		
+	}
+	
+	/**
+	 * Only DataFlowControlAction objects can be actions for this button
+	 * @see javax.swing.AbstractButton#setAction(javax.swing.Action)
+	 * @throws IllegalArgumentException if the action is not a DataFlowControlAction object
+	 */
+	public void setAction(Action a)
+	{
+		if (a == null){
+			throw new IllegalArgumentException("Action cannot be set to null.");
+		}
+		
+		//Only allow DataFlowControlAction objects
+		if (!(a instanceof DataFlowControlAction)){
+			throw new IllegalArgumentException("Wrong Action class:" + a.getClass().getName() +
+					".The action for a DataFlowControlButton has to be a DataFlowControlAction.");
+		}
+		
+		setDataFlowControlAction((DataFlowControlAction)a);
+	}
+	
+	/**
+	 * Returns the DataFlowControlAction associated with this button
+	 * Guaranteed to be non-null
+	 */
+	public DataFlowControlAction getDataFlowControlAction()
+	{
+		return (DataFlowControlAction)getAction();
+	}
+	
+	/**
+	 * Returns the DataFlowControlAction associated with this button
+	 * Guaranteed to be non-null
+	 */
+	public void setDataFlowControlAction(DataFlowControlAction action)
+	{
+		super.setAction(action);
+	}
+}
---
+++ src/org/concord/data/ui/DataFlowControlToolBar.java
@@ -0,0 +1,129 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2004-09-22 16:28:58 $
+ * $Author: imoncada $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.data.ui;
+
+import java.awt.Component;
+import java.util.Vector;
+
+import javax.swing.JToolBar;
+
+import org.concord.framework.data.DataFlow;
+
+
+/**
+ * DataFlowControlToolBar
+ * Class name and description
+ *
+ * Date created: Sep 20, 2004
+ *
+ * @author imoncada<p>
+ *
+ */
+public class DataFlowControlToolBar extends JToolBar
+{
+	protected Vector objsFlow;				//DataFlow objects
+
+	/**
+	 * Default constructor
+	 * Creates by default START, STOP and RESET buttons
+	 */
+	public DataFlowControlToolBar()
+	{
+		this(true);
+	}
+		
+	/**
+	 * Creates by default START, STOP and RESET buttons
+	 * acting on the data flow object specified
+	 * @param objFlow
+	 */
+	public DataFlowControlToolBar(DataFlow objFlow)
+	{
+		this();
+		addDataFlowObject(objFlow);
+	}
+	
+	/**
+	 * If bCreateDefaultButtons is true,
+	 * creates by default START, STOP and RESET buttons
+	 * @param bCreateDefaultButtons
+	 */
+	public DataFlowControlToolBar(boolean bCreateDefaultButtons)
+	{
+		super();
+		objsFlow = new Vector();
+		if (bCreateDefaultButtons){
+			addDataFlowControlButton(DataFlowControlAction.FLOW_CONTROL_START);
+			addDataFlowControlButton(DataFlowControlAction.FLOW_CONTROL_STOP);
+			addDataFlowControlButton(DataFlowControlAction.FLOW_CONTROL_RESET);
+		}
+	}
+	
+	public void addDataFlowControlButton(int type)
+	{
+		DataFlowControlButton b = new DataFlowControlButton(type);
+		add(b);
+	}
+	
+	public void add(DataFlowControlButton b)
+	{
+		super.add(b);
+		addDataFlowObjects(b);
+	}
+	
+	/**
+	 * Adds a DataFlow object that will be controlled by this button
+	 * @param objFlow The data flow or simulation object to control
+	 */
+	public void addDataFlowObject(DataFlow objFlow)
+	{
+		if (objsFlow.contains(objFlow)) return;
+		objsFlow.addElement(objFlow);
+		
+        int n = this.getComponentCount();
+        if (n <= 0) return;
+        Component[] comps = this.getComponents();
+        DataFlowControlButton b;
+        for ( int i = 0; i < n; i++) {
+        	if (comps[i] instanceof DataFlowControlButton){
+        		b = (DataFlowControlButton)comps[i];
+        		b.addDataFlowObject(objFlow);
+        	}
+        }
+	}
+
+	/**
+	 * Removes a DataFlow object that was previously controlled by this button
+	 * @param objFlow The data flow or simulation object to remove and not be controlled anymore
+	 */
+	public void removeDataFlowObject(DataFlow objFlow)
+	{
+		objsFlow.removeElement(objFlow);
+		
+        int n = this.getComponentCount();
+        if (n <= 0) return;
+        Component[] comps = this.getComponents();
+        DataFlowControlButton b;
+        for ( int i = 0; i < n; i++) {
+        	if (comps[i] instanceof DataFlowControlButton){
+        		b = (DataFlowControlButton)comps[i];
+        		b.removeDataFlowObject(objFlow);
+        	}
+        }
+	}
+	
+	protected void addDataFlowObjects(DataFlowControlButton b)
+	{
+		for (int i=0; i<objsFlow.size(); i++){
+			b.addDataFlowObject((DataFlow)objsFlow.elementAt(i));
+		}
+	}
+	
+}
