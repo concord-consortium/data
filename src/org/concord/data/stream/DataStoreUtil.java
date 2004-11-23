08f3e5ec12e48ae2ed984b51463bee032fd9f141
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2004-11-23 16:09:07 +0000

    Add method for printing datastore as a tab delemeted file.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@2119 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/stream/DataStoreUtil.java
+++ src/org/concord/data/stream/DataStoreUtil.java
@@ -1,7 +1,7 @@
 /*
  * Last modification information:
- * $Revision: 1.1 $
- * $Date: 2004-11-16 18:48:07 $
+ * $Revision: 1.2 $
+ * $Date: 2004-11-23 16:09:07 $
  * $Author: scytacki $
  *
  * Licence Information
@@ -11,9 +11,12 @@ package org.concord.data.stream;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.Reader;
 import java.util.StringTokenizer;
 
+import org.concord.framework.data.stream.DataChannelDescription;
+import org.concord.framework.data.stream.DataStore;
 import org.concord.framework.data.stream.WritableDataStore;
 
 
@@ -32,6 +35,8 @@ public class DataStoreUtil
 		throws IOException
 	{
 		BufferedReader lineReader = new BufferedReader(reader);
+
+		int numChannels = ds.getTotalNumChannels();
 		
 		if(hasHeaders) {
 			// parse the headers and set the channel descriptions
@@ -39,14 +44,15 @@ public class DataStoreUtil
 
 		int row = 0;
 		int col = 0;
-
+		
 		String line = lineReader.readLine();
 		while(line != null) {
 			col = 0;
 			StringTokenizer toks = new StringTokenizer(line, "\t");
-
+			
 			while(toks.hasMoreTokens()) {
 				String strValue = toks.nextToken();
+				
 				try {
 					Float fValue = Float.valueOf(strValue);
 					ds.setValueAt(row, col, fValue);
@@ -60,4 +66,47 @@ public class DataStoreUtil
 			line = lineReader.readLine();
 		}		
 	}
+	
+	public static void printData(PrintStream outS, DataStore ds, 
+			int rowsToPrint[], boolean printHeaders)
+	{
+		int numRows, numCols;
+		numRows = ds.getTotalNumSamples();
+		numCols = ds.getTotalNumChannels();
+
+		if (printHeaders){
+			for (int j=0; j<numCols; j++){
+				DataChannelDescription cDesc = ds.getDataChannelDescription(j);
+				outS.print(cDesc.getName()+"\t");
+			}
+			outS.println("");
+		}
+
+		int r = 0;
+		boolean blnPrint;
+		for (int i=0; i<numRows; i++){
+			blnPrint = false;
+			if (rowsToPrint == null){
+				blnPrint = true;
+			}
+			else {
+				if (r < rowsToPrint.length && rowsToPrint[r] == i){
+					blnPrint = true;
+					r++;
+				}
+			}
+			if (blnPrint){
+				Object obj;
+				for (int j=0; j<numCols; j++){
+					obj = ds.getValueAt(i, j);
+					if (obj != null){
+						outS.print(obj.toString());
+					}
+					outS.print("\t");
+				}
+				outS.println("");
+			}
+		}
+	}
+
 }
