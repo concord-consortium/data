721f16beef43e7384b02c7417f9a709f243f7f04
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-03-07 04:10:20 +0000

    removed dataproducerproxy reference
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/data/data@3489 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/state/OTDataFieldView.java
+++ src/org/concord/data/state/OTDataFieldView.java
@@ -42,7 +42,6 @@ import org.concord.data.ui.DataFlowControlToolBar;
 import org.concord.data.ui.DataStoreLabel;
 import org.concord.data.ui.DataValueLabel;
 import org.concord.framework.data.stream.DataProducer;
-import org.concord.framework.data.stream.DataProducerProxy;
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.view.OTObjectView;
 import org.concord.framework.otrunk.view.OTViewContainer;
@@ -81,14 +80,7 @@ public class OTDataFieldView
 			return dataStoreField;
 		}
 		
-		DataProducer dataProducer = null;
-		OTObject dataProducerObj = otObject.getDataProducer();
-		if(dataProducerObj instanceof DataProducerProxy)
-		{
-			dataProducer = ((DataProducerProxy)dataProducerObj).getDataProducer();
-		} else {
-			dataProducer = (DataProducer)dataProducerObj;
-		}
+		DataProducer dataProducer = (DataProducer)otObject.getDataProducer();
 		
 		dataField = new DataValueLabel(dataProducer);
 
