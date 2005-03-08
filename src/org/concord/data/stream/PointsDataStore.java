59f7654f72e5e311a191040764044ac52ad98576
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2005-03-08 08:54:56 +0000

    Added Line Type Function for the drawing data graphable that only works with PointsDataStore! (for now).
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@3533 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/stream/PointsDataStore.java
+++ src/org/concord/data/stream/PointsDataStore.java
@@ -24,14 +24,16 @@
  */
 /*
  * Last modification information:
- * $Revision: 1.2 $
- * $Date: 2004-11-12 19:43:27 $
- * $Author: eblack $
+ * $Revision: 1.3 $
+ * $Date: 2005-03-08 08:54:56 $
+ * $Author: imoncada $
  *
  * Licence Information
  * Copyright 2004 The Concord Consortium 
 */
 package org.concord.data.stream;
+
+import java.util.Vector;
 
 import org.concord.framework.data.stream.DefaultDataStore;
 
@@ -57,5 +59,201 @@ public class PointsDataStore extends DefaultDataStore
 		int sample = getTotalNumSamples();
 		setValueAt(sample, 0, new Float(x));
 		setValueAt(sample, 1, new Float(y));
+	}
+
+	public void addPointOrderFromTo(float x, float y, float otherX)
+	{
+		if (otherX <= x){
+			addPointOrderFrom(x, y, otherX);
+		}
+		else{
+			addPointOrderTo(x, y, otherX);
+		}
+	}
+	
+	public void addPointOrderFrom(float x, float y, float startX)
+	{
+		float value;
+		int i;
+		int startI;
+		int t;
+		int ti;
+		
+		if (x < startX){
+			return;
+		}
+		if (x == startX){
+			addPointOrder(x, y);
+			return;
+		}
+		
+		i = getIndexValue(x);
+		t = getTotalNumSamples();
+		startI = getIndexValue(startX);
+		value = getXValue(i);
+		
+		//System.out.println("addPointOrderFrom("+x+" "+i+" "+startX+" "+startI+") in "+getFloatVectorStr((Vector)channelsValues.elementAt(0)));
+		
+		//Removing all values from startI to i
+		ti = Math.min(i, t);
+		for (int ii = startI+1; ii < ti; ii++){
+			for (int j=0; j < channelsValues.size(); j++){
+				Vector channel = (Vector)channelsValues.elementAt(j);
+				channel.remove(startI+1);
+			}
+			i--;
+		}
+		//i = i - (ti - (startI +1));
+		
+		//System.out.println("after removing, i:"+i+" "+getFloatVectorStr((Vector)channelsValues.elementAt(0)));
+		
+		addPointOrder(x, y, i, value);
+		
+	}
+	
+	public void addPointOrderTo(float x, float y, float endX)
+	{
+		float value;
+		int i;
+		int endI;
+		int t;
+		int ti;
+		
+		if (x > endX){
+			return;
+		}
+		if (x == endX){
+			addPointOrder(x, y);
+			return;
+		}
+		
+		i = getIndexValue(x);
+		t = getTotalNumSamples();
+		endI = getIndexValue(endX);
+		value = getXValue(i);
+		
+		//System.out.println("addPointOrderFrom("+x+" "+i+" "+endX+" "+endI+") in "+getFloatVectorStr((Vector)channelsValues.elementAt(0)));
+		
+		//Removing all values from i to endI
+		ti = Math.min(endI, t);
+		for (int ii = i; ii < ti; ii++){
+			for (int j=0; j < channelsValues.size(); j++){
+				Vector channel = (Vector)channelsValues.elementAt(j);
+				channel.remove(i);
+			}
+		}
+		
+		//System.out.println("after removing, i:"+i+" "+getFloatVectorStr((Vector)channelsValues.elementAt(0)));
+		
+		addPointOrder(x, y, i, value);
+	}
+	
+	public void addPointOrder(float x, float y)
+	{
+		float value;
+		int i = getIndexValue(x);
+		value = getXValue(i);
+		
+		addPointOrder(x, y, i, value);
+	}
+	
+	/**
+	 * @param x
+	 * @param i
+	 * @param value
+	 */
+	private void addPointOrder(float x, float y, int i, float value)
+	{
+		if (i < getTotalNumSamples()){
+			for (int j=0; j < channelsValues.size(); j++){
+				Vector channel = (Vector)channelsValues.elementAt(j);
+				if (value == x){
+					if (j == 0){
+						channel.set(i, new Float(x));
+						//System.out.println("addPointOrder set "+getFloatVectorStr(channel));
+					}
+					else if (j == 1){
+						channel.set(i, new Float(y));
+					}
+					else{
+						channel.set(i, null);
+					}
+				}
+				else{
+					if (j == 0){
+						channel.add(i, new Float(x));
+						//System.out.println("addPointOrder add "+getFloatVectorStr(channel));
+					}
+					else if (j == 1){
+						channel.add(i, new Float(y));
+					}
+					else{
+						channel.add(i, null);
+					}
+				}
+			}
+			notifyDataChanged();
+		}
+		else{
+			addPoint(x,y);
+		}
+	}
+
+	/**
+	 * @param channel
+	 * @return
+	 */
+	private String getFloatVectorStr(Vector v)
+	{
+		String strS = "[";
+		for (int i=0; i<v.size(); i++){
+			if (i > 0){
+				strS = strS + ",";
+			}
+			Object obj = v.elementAt(i);
+			if (obj instanceof Float){
+				Float f = (Float)obj;
+				strS = strS + f.floatValue();
+			}
+		}
+		strS = strS + "]";
+		return strS;
+	}
+
+	/**
+	 * @param i
+	 * @return
+	 */
+	private float getXValue(int i)
+	{
+		Object obj = getValueAt(i, 0);
+		if (obj instanceof Float){
+			return ((Float)obj).floatValue();
+		}
+		return Float.NaN;
+	}
+
+	/**
+	 * @param x
+	 * @return
+	 */
+	private int getIndexValue(float x)
+	{
+		float value;
+		Object obj;
+		int i;
+		for (i = 0; i < getTotalNumSamples(); i++){
+			obj = getValueAt(i, 0);
+			if (obj instanceof Float){
+				value = ((Float)obj).floatValue();
+				if (value >= x){
+					break;
+				}
+			}
+		}
+		
+		//System.out.println("get index "+i+" of value "+x+" in "+getFloatVectorStr((Vector)channelsValues.elementAt(0)));
+		
+		return i;
 	}
 }
