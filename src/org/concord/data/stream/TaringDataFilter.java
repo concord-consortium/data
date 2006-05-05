ace7be4af2577264cb6324a0615f3d632645c2b4
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2006-05-05 15:48:54 +0000

    implemented method in datastore to take in a full array of values at once instead of one at a time.  Added a tarring data filter, this can be used to zero out a stream of data.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/data/data@6091 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/data/stream/TaringDataFilter.java
@@ -0,0 +1,233 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2006-05-05 15:48:54 $
+ * $Author: scytacki $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.data.stream;
+
+import java.util.Vector;
+
+import org.concord.framework.data.stream.DataConsumer;
+import org.concord.framework.data.stream.DataListener;
+import org.concord.framework.data.stream.DataProducer;
+import org.concord.framework.data.stream.DataStreamDescription;
+import org.concord.framework.data.stream.DataStreamEvent;
+
+/**
+ * TaringDataFilter
+ * Class name and description
+ *
+ * Date created: Jun 2, 2005
+ *
+ * @author scott<p>
+ *
+ */
+public class TaringDataFilter
+    implements DataProducer, DataConsumer
+{
+    private Vector dataListeners = new Vector();
+    private DataProducer source;
+    private boolean doingTare = false;
+    private DataStreamEvent dataEvent;
+    private DataListener dataListener;
+
+    protected float tareOffset = 0;
+    protected int tareChannel = 0;
+    
+    public TaringDataFilter()
+    {
+        /*
+        * Send the data listening to this inner class so
+        * it hides that from users of this class.
+        */
+        this.dataListener = new DataListener() {
+            public void dataReceived(DataStreamEvent dataEvent)
+            {
+                TaringDataFilter.this.dataReceived(dataEvent);
+            }  
+
+            public void dataStreamEvent(DataStreamEvent dataEvent)
+            {
+                TaringDataFilter.this.dataStreamEvent(dataEvent);
+            }
+        };
+    }
+    
+    /**
+     * set the channel number that will be adjusted by the tare
+     * this uses the virtual channel like is used by the DataGraphable
+     * class.  So channel 0 means the first channel.  Even if the 
+     * DataProducer has a dt which makes the first channel -1.  If the
+     * DataProducer doesn't have a dt then the first channel is 0.  
+     * 
+     * @param channelNumber
+     */
+    public void setChannel(int channelNumber)
+    {
+        // set the channel number 
+        tareChannel = channelNumber;
+    }
+    
+    protected int getProducerChannel()
+    {
+        if(source == null) {
+            throw new RuntimeException("Null source");
+        }
+        
+        if(source.getDataDescription().getDataType() ==
+            DataStreamDescription.DATA_SEQUENCE) {
+            // has a dt
+            return tareChannel - 1;
+        } else {
+            return tareChannel;
+        }
+    }
+    
+    protected void dataReceived(DataStreamEvent dataEvent)
+    {
+        if(doingTare) {
+            // save this for the calibration
+            
+        } else {
+            // send the event to our listeners
+            // however we need to do the actual filter
+            // here and adjust the values
+            int producerChannel = getProducerChannel();
+            dataEvent.getNumSamples();
+            notifyDataReceived(dataEvent);
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataListener#dataStreamEvent(org.concord.framework.data.stream.DataStreamEvent)
+     */
+    protected void dataStreamEvent(DataStreamEvent dataEvent)
+    {
+        if(doingTare) {
+            // save this for the calibration
+        } else {
+            // send the event to our listeners
+            notifyDataStreamEvent(dataEvent);
+        }
+    }
+            
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataProducer#addDataListener(org.concord.framework.data.stream.DataListener)
+     */
+    public void addDataListener(DataListener listener)
+    {
+        // we need our own list listeners so that we can shield 
+        // these listeners when we do the calibration run
+        dataListeners.add(listener);
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataProducer#removeDataListener(org.concord.framework.data.stream.DataListener)
+     */
+    public void removeDataListener(DataListener listener)
+    {
+        // TODO Auto-generated method stub
+        dataListeners.remove(listener);
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataProducer#getDataDescription()
+     */
+    public DataStreamDescription getDataDescription()
+    {
+        // TODO Auto-generated method stub
+        if(source == null) {
+            return null;
+        }
+
+        return source.getDataDescription();
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataConsumer#addDataProducer(org.concord.framework.data.stream.DataProducer)
+     */
+    public void addDataProducer(DataProducer source)
+    {
+        if(source == null) {
+            return;
+        }
+        
+        if(this.source != null) {
+            this.source.removeDataListener(dataListener);
+        }
+        this.source = source;
+        this.source.addDataListener(dataListener);
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.stream.DataConsumer#removeDataProducer(org.concord.framework.data.stream.DataProducer)
+     */
+    public void removeDataProducer(DataProducer source)
+    {
+        if(this.source == source) {
+            this.source.removeDataListener(dataListener);            
+            this.source = null;
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.DataFlow#reset()
+     */
+    public void reset()
+    {
+        if(source != null) {
+            source.reset();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.DataFlow#stop()
+     */
+    public void stop()
+    {
+        if(source != null) {
+            source.stop();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.concord.framework.data.DataFlow#start()
+     */
+    public void start()
+    {
+        if(source != null) {
+            source.start();
+        }
+    }
+
+	protected void notifyDataReceived(DataStreamEvent sourceEvent)
+	{
+	    sourceEvent.clone(dataEvent);
+	    dataEvent.setSource(this);
+		for(int i=0; i<dataListeners.size(); i++) {
+			DataListener dataListener = (DataListener)dataListeners.elementAt(i);
+			if(dataListener != null)
+			{
+				dataListener.dataReceived(dataEvent);
+			}
+		}
+	}
+	
+	protected void notifyDataStreamEvent(DataStreamEvent sourceEvent)
+	{
+	    sourceEvent.clone(dataEvent);
+	    dataEvent.setSource(this);
+		for(int i=0; i<dataListeners.size(); i++) {
+			DataListener dataListener = (DataListener)dataListeners.elementAt(i);
+			if(dataListener != null)
+			{
+				dataListener.dataStreamEvent(dataEvent);
+			}
+		}
+	}
+}
