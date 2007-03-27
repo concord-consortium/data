ee212b3afb138eba18571a071e88e84a98016164
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2007-03-27 05:41:12 +0000

    Stupid little equals method added. To be corrected by Scott.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/data/data@7131 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/MathUtil.java
+++ src/org/concord/data/MathUtil.java
@@ -33,11 +33,21 @@ package org.concord.data;
  */
 public final class MathUtil {
     public static boolean equalWithTolerance(float a,float b,float eps){
-        if(a == 0) return Math.abs(a) < eps;
+        if(a == 0) return Math.abs(b) < eps;
         return Math.abs((a-b)/a) < eps;
     }
     public static boolean equalWithTolerance(double a,double b,double eps){
-        if(a == 0) return Math.abs(a) < eps;
+        if(a == 0) return Math.abs(b) < eps;
         return Math.abs((a-b)/a) < eps;
+    }
+    public static boolean equals(float a, float b){
+    	if (b == 0) return equals(a+1, b+1);
+    	float c = a/b;
+    	return equalWithTolerance(c, 1, 0.00001);
+    }
+    public static boolean equals(double a, double b){
+    	if (b == 0) return equals(a+1, b+1);
+    	double c = a/b;
+    	return equalWithTolerance(c, 1, 0.00001);
     }
 }
