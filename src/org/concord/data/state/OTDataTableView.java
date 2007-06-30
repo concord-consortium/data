f4cc79f06d5021894b9993f1a4eff155bd3c6845
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2007-06-30 03:59:21 +0000

    Added OT data column description. Color doesn't work yet.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@7808 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/state/OTDataTableView.java
+++ src/org/concord/data/state/OTDataTableView.java
@@ -1,20 +1,25 @@
 /*
  * Last modification information:
- * $Revision: 1.2 $
- * $Date: 2007-06-25 18:59:12 $
- * $Author: scytacki $
+ * $Revision: 1.3 $
+ * $Date: 2007-06-30 03:59:21 $
+ * $Author: imoncada $
  *
  * Licence Information
  * Copyright 2004 The Concord Consortium 
 */
 package org.concord.data.state;
 
+import java.awt.Color;
+
 import javax.swing.JComponent;
 
+import org.concord.data.ui.DataColumnDescription;
+import org.concord.data.ui.DataTableModel;
 import org.concord.data.ui.DataTablePanel;
 import org.concord.framework.data.stream.DataStore;
 import org.concord.framework.otrunk.OTControllerService;
 import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.OTObjectList;
 import org.concord.framework.otrunk.view.OTJComponentView;
 
 
@@ -50,7 +55,32 @@ public class OTDataTableView
 		dataStore = (DataStore) controllerService.getRealObject(otDataStore);
 		table.getTableModel().addDataStore(dataStore);
 		
+		updateOTColumns(table.getTableModel(), dataStore, otTable.getColumns());
+		
 		return table;
+	}
+
+	/**
+	 * @param dataStore
+	 * @param columns
+	 */
+	protected void updateOTColumns(DataTableModel tableModel, DataStore dataStore, OTObjectList columns)
+	{
+		for (int i = 0; i < columns.size(); i++){
+			OTDataColumnDescription otColDesc = (OTDataColumnDescription)columns.get(i); 
+			if (otColDesc == null) continue;
+			DataColumnDescription colDesc = tableModel.getDataColumn(dataStore, i);
+			
+			String label = otColDesc.getLabel();
+			if (label != null){
+				colDesc.setLabel(label);
+			}
+			if (otColDesc.isResourceSet("color")){
+				System.out.println("set color to "+otColDesc.getColor());
+				colDesc.setColor(new Color(otColDesc.getColor()));
+			}
+		}
+		tableModel.fireTableStructureChanged();
 	}
 
 	/**
