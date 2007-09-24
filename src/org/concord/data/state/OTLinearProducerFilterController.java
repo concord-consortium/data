311ce5d2977f64bdc0b36ecb66f5ebe926308a40
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-09-24 16:55:09 +0000

    Added listener support so changing the ot filter updates the real object immedidately.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@8265 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/state/OTLinearProducerFilterController.java
+++ src/org/concord/data/state/OTLinearProducerFilterController.java
@@ -4,6 +4,9 @@
 package org.concord.data.state;
 
 import org.concord.data.stream.LinearProducerFilter;
+import org.concord.framework.otrunk.OTChangeEvent;
+import org.concord.framework.otrunk.OTChangeListener;
+import org.concord.framework.otrunk.OTChangeNotifying;
 
 /**
  * @author scott
@@ -12,7 +15,9 @@ import org.concord.data.stream.LinearProducerFilter;
 public class OTLinearProducerFilterController extends OTDataProducerFilterController
 {
 	public static Class [] realObjectClasses =  {LinearProducerFilter.class};	
-	public static Class otObjectClass = OTLinearProducerFilter.class;    
+	public static Class otObjectClass = OTLinearProducerFilter.class;
+	private OTChangeListener changeListener;
+	private LinearProducerFilter realObject;    
 	
 	/* (non-Javadoc)
 	 * @see org.concord.framework.otrunk.OTController#loadRealObject(java.lang.Object)
@@ -42,4 +47,22 @@ public class OTLinearProducerFilterController extends OTDataProducerFilterContro
 		otFilter.setK1(filter.getK1());
 	}
 	
+	public void registerRealObject(Object realObject) 
+	{
+		this.realObject = (LinearProducerFilter) realObject;
+		
+		super.registerRealObject(realObject);
+		changeListener = new OTChangeListener(){
+			public void stateChanged(OTChangeEvent e) 
+			{
+				OTLinearProducerFilter otFilter = (OTLinearProducerFilter) otObject;
+				LinearProducerFilter filter = OTLinearProducerFilterController.this.realObject;
+				
+				filter.setK0(otFilter.getK0());
+				filter.setK1(otFilter.getK1());
+			}
+			
+		};
+		((OTChangeNotifying)otObject).addOTChangeListener(changeListener);
+	}
 }
