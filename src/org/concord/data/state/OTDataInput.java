b5d5632f2fa3e1bfa1cb77c0d4b5ccfcea467a9f
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2008-04-10 18:01:10 +0000

    Added target to OTDataInput.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/data/data@13220 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/state/OTDataInput.java
+++ src/org/concord/data/state/OTDataInput.java
@@ -1,26 +1,39 @@
 package org.concord.data.state;
 
+import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectInterface;
 
 /**
- * This class is used to add a data producer to a model (or anything else) with the
- * inclusion of named variables or channels which will accept the input data. 
- * It is currently used by OTModelerPage and OTNLogoModel.
+ * This class is used to add a data producer to a model (or anything else) with
+ * the inclusion of named variables or channels of a specified target which will
+ * accept the input data. It is currently used by OTModelerPage and
+ * OTNLogoModel. If the target is not specified, then the input variable is
+ * assumed to be associated to the entire model (QX).
  * 
  * @author sfentress
- *
+ * 
  */
-public interface OTDataInput extends OTObjectInterface
-{
+public interface OTDataInput extends OTObjectInterface {
+
 	public OTDataProducer getDataProducer();
+
 	public void setDataProducer(OTDataProducer producer);
-	
-	//optional
+
+	// optional (QX)
+	public OTObject getTarget();
+
+	public void setTarget(OTObject target);
+
+	// optional
 	public String getInputVariable();
+
 	public void setInputVariable(String inputVariable);
-	
-	//optional
+
+	// optional
 	public int getInputChannel();
+
 	public void setInputChannel(int inputChannel);
+
 	public static int DEFAULT_inputChannel = 0;
+
 }
