265415f0098792d867dab72ab21b3745a3f7b7be
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2008-10-30 13:55:12 +0000

    Update the DataSlopeFilter so that it can display filtered data while the inputDataStore is still being written to.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@16084 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/state/filter/DataSlopeFilter.java
+++ src/org/concord/data/state/filter/DataSlopeFilter.java
@@ -72,6 +72,30 @@ public class DataSlopeFilter extends AbstractDataStoreFilter
 			//This data store has 3 channels: x, y and dy/dx
 			return 3;
 		}
+		
+		/**
+		 * @see org.concord.framework.data.stream.DataStore#getTotalNumSamples()
+		 */
+		public int getTotalNumSamples()
+		{
+			/* 
+			 * Because this filter uses the sample before and the sample after the current sample to calculate
+			 * the slope, until we get 3 samples, we can't provide any useful data. Even then, we can only
+			 * provide useful data for one sample less than the actual number of raw samples.
+			 * 
+			 * It is required to do it this way if you want to display a filtered dataStore while it is still being
+			 * written into.
+			 * 
+			 * Drawbacks:
+			 *  - If you have less than 3 samples, you won't get any samples.
+			 *  - You won't get the original data for the last sample of the source dataStore.
+			 */
+			int samples = dataFilter.getInputDataStore().getTotalNumSamples();
+			if (samples < 3) {
+				return 0;
+			}
+			return samples - 1;
+		}
 
 		/**
 		 * @see org.concord.framework.data.stream.DataStore#getValueAt(int, int)
