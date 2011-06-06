3c354f68963c964db372d3843092082063f65a06
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-06-06 17:46:18 +0000

    Discard samples received while our source is not running.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/data@24822 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/data/stream/SingleYPerXProducerFilter.java
+++ src/org/concord/data/stream/SingleYPerXProducerFilter.java
@@ -22,50 +22,53 @@ public class SingleYPerXProducerFilter extends DataProducerFilter {
     @Override
     protected void dataReceived(DataStreamEvent dataEvent)
     {
-        boolean notify = false;
-        if(source.getDataDescription().getDataType() == DataStreamDescription.DATA_SEQUENCE) {
-            this.dataEvent.data = dataEvent.data;
-            this.dataEvent.setNumSamples(dataEvent.getNumSamples());
-            notify = true;
-        } else {
-            int numSamples = dataEvent.getNumSamples();
-            int offset = dataEvent.getDataDescription().getDataOffset();
-            int stride = dataEvent.getDataDescription().getNextSampleOffset();
-            int channelsPerSample = dataEvent.getDataDescription().getChannelsPerSample();
+        if (isRunning()) {
+            boolean notify = false;
             
-            // do the actual filtering
-            int filteredOffset = 0;
-            int filteredSampleCount = 0;
-            for(int i=0; i<numSamples; i++){
-                float currentX = dataEvent.data[offset+timeChannel];
-                if (! Float.isNaN(currentX)) {
-                    if (lastX != currentX) {
-                        // copy the values for the lastX into the dataEvent
-                        for(int j=0; j<channelsPerSample; j++){
-                            this.dataEvent.data[filteredOffset+j] = lastValues[j];
-                        }
-                        
-                        if (! Float.isNaN(lastX)) {
-                            notify = true;
-                        }
-                        lastX = currentX;
-                        filteredOffset += stride;
-                        filteredSampleCount++;
-                    }
-                }
+            if(source.getDataDescription().getDataType() == DataStreamDescription.DATA_SEQUENCE) {
+                this.dataEvent.data = dataEvent.data;
+                this.dataEvent.setNumSamples(dataEvent.getNumSamples());
+                notify = true;
+            } else {
+                int numSamples = dataEvent.getNumSamples();
+                int offset = dataEvent.getDataDescription().getDataOffset();
+                int stride = dataEvent.getDataDescription().getNextSampleOffset();
+                int channelsPerSample = dataEvent.getDataDescription().getChannelsPerSample();
                 
-                // copy the current values into the placeholder
-                for(int j=0; j<channelsPerSample; j++){
-                    lastValues[j] = dataEvent.data[offset+j];
-                }
+                // do the actual filtering
+                int filteredOffset = 0;
+                int filteredSampleCount = 0;
+                for(int i=0; i<numSamples; i++){
+                    float currentX = dataEvent.data[offset+timeChannel];
+                    if (! Float.isNaN(currentX)) {
+                        if (lastX != currentX) {
+                            // copy the values for the lastX into the dataEvent
+                            for(int j=0; j<channelsPerSample; j++){
+                                this.dataEvent.data[filteredOffset+j] = lastValues[j];
+                            }
                             
-                offset += stride;
+                            if (! Float.isNaN(lastX)) {
+                                notify = true;
+                            }
+                            lastX = currentX;
+                            filteredOffset += stride;
+                            filteredSampleCount++;
+                        }
+                    }
+                    
+                    // copy the current values into the placeholder
+                    for(int j=0; j<channelsPerSample; j++){
+                        lastValues[j] = dataEvent.data[offset+j];
+                    }
+                                
+                    offset += stride;
+                }
+                this.dataEvent.setNumSamples(filteredSampleCount);
             }
-            this.dataEvent.setNumSamples(filteredSampleCount);
-        }
-        
-        if (notify) {
-            notifyDataReceived();
+            
+            if (notify) {
+                notifyDataReceived();
+            }
         }
     }
     
